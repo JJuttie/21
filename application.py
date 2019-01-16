1eeaed94eaa0affb488bcca3151a721c0a384f94
Mike van Gils <mikevangils1@gmail.com>
2019-01-16 15:21:19 +0000

    e

--- application.py
+++ application.py
@@ -82,7 +82,55 @@ def logout():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    return register2()
+    """Register user."""
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # provide email
+        if not request.form.get("email"):
+            return apology("You must provide an email.")
+
+        # provide name
+        elif not request.form.get("name"):
+            return apology("must provide your name")
+
+        # provide town
+        elif not request.form.get("town"):
+            return apology("must provide your town")
+
+        # provide password
+        elif not request.form.get("password"):
+            return apology("must provide a password")
+
+        # provide password again
+        elif not request.form.get("confirmation"):
+            return apology("must provide a confirmation password")
+
+        # checken
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("wachtwoorden zijn niet gelijk aan elkaar")
+
+        #wachtwoord encrypten
+        password=pwd_context.hash(request.form.get("password"))
+
+        # pomp het in de database
+        gebruiker = db.execute("INSERT INTO users(email, name, town, hash) VALUES(:email, :name, :town, :hash)",
+        email=request.form.get("email"), name=request.form.get("name"),
+        town=request.form.get("town"), hash=password)
+
+        #email al in gebruik
+        if not gebruiker:
+            return apology("eMail has already been used")
+
+        #nu ingelogd:
+        session["user-id"] = gebruiker
+        # redirect user to home page
+        return redirect(url_for("recipe"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
 
 #extra_opdracht2
 @app.route("/password", methods=["GET", "POST"])
