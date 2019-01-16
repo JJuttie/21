b44cfe9ef886c1102aa86838ac955460da405989
Alex Hakvoort <alex.h@hotmail.nl>
2019-01-16 12:00:10 +0000

    changes

--- Alex.py
+++ Alex.py
@@ -3,3 +3,10 @@ import urllib.request
 from cs50 import SQL
 from flask import redirect, render_template, request, session
 from functools import wraps
+
+def recipe():
+    if request.method == "POST":
+        title = request.method.get("title")
+        bio = request.method.get("bio")
+        tages = request.method.get("tags")
+
