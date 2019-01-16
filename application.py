7997cabf60911e1b8d901043c6b63b10eb7047b2
Mike van Gils <mikevangils1@gmail.com>
2019-01-16 17:22:10 +0000

    recipe registratie

--- application.py
+++ application.py
@@ -126,7 +126,7 @@ def register():
         #nu ingelogd:
         session["user-id"] = gebruiker
         # redirect user to home page
-        return render_template("recipe.html")
+        return redirect(url_for("recipe"))
 
     # else if user reached route via GET (as by clicking a link or via redirect)
     else:
