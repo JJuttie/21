c0126e14964004ce6bd0309b7a7ade40003c268b
Mike van Gils <mikevangils1@gmail.com>
2019-01-16 17:29:06 +0000

    recipes worden nog niet toegevoegd aan de database

--- application.py
+++ application.py
@@ -66,7 +66,7 @@ def login():
         session["user_id"] = rows[0]["id"]
 
         # redirect user to home page
-        return render_template("recipe.html")
+        return render_template("index.html")
 
     # else if user reached route via GET (as by clicking a link or via redirect)
     else:
