c0e9ecc6a219ef7456a23c78c93cf1c562441a18
Jasmine <jasmineevers24@gmail.com>
2019-01-17 10:34:02 +0000

    delete forms

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms import SpringField, PasswordField
-from wtforms.validators import DataRequired, Email
-
-class EmailForm(Form):
-    email = TextField('Email', validators=[DataRequired(), Email()])
-
-class PasswordForm(Form):
-    password = PasswordField('Email', validators=[DataRequired()])
